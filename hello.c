54a2b865f745ba4d3b1362729c6708a37a7ddc03
KoleBrack <kbrack2@huskers.unl.edu>
2021-08-27 21:06:51 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Kole Brack
+ *         kbrack2@huskers.unl.edu
+ * Date: 2021/08/27
+ * 
+ * Simple C program that prints my name
+ * 
+ */
+ *#include <stdlib.h>
+ *#include <stdio.h>
+ *
+ int main(int argc, char **argv); {
+     
+     printf("Kole Brack\n");
+ }
